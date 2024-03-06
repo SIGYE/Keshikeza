d0cbda5d2736a79759120a631e6dd94512b6fc62
Mwesigye Linda <lindamwesigye2@gmail.com>
2024-03-06 10:14:10 +0200

    about-us + links

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import gorilla from '../../assets/gorilla.jpg'
 import Line4 from '../../assets/Line 4.png'
 import './About.css'
+import {arrow-right} from 'react-icons'
 
 const About = () => {
   return (
@@ -17,13 +18,13 @@ const About = () => {
       <h1>Welcome to <span>KENSHIKEZA</span></h1>
       <p>Kenshikeza, an innovative online portal, stands as the epitome of seamless travel experiences, <br />
              offering a comprehensive platform that centers around tourism and the sale of related products. <br />
-             Whether you're seeking enchanting destinations, curated travel packages, or memorable excursions,<br />
-             Kenshikeza caters to your wanderlust with a diverse array of options. Beyond its focus on travel, <br />
-              the portal elevates the user experience by facilitating hassle-free bookings for top-notch restaurants <br />
-             and accommodations. With Kenshikeza, users can effortlessly plan their entire journey, from exploring <br />
-             captivating landmarks to securing reservations at the finest hotels and dining establishments. <br />
+           Beyond its focus on travel, the portal elevates the user experience by facilitating hassle-free bookings <br />
+            for top-notch restaurants and accommodations. 
          </p>
     </div>
+      <div className="dest-buttons">
+        <a href="#">First Class </a>
+      </div>
     </div>
     
     </>
