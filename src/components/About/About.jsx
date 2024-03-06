9ca2b4db54adf64e039f1100d588bfd53d80085b
Mwesigye Linda <lindamwesigye2@gmail.com>
2024-03-06 09:30:52 +0200

    about-us +  text content

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,7 +15,14 @@ const About = () => {
       <h2>ABOUT US</h2>
       <div className="welcome">
       <h1>Welcome to <span>KENSHIKEZA</span></h1>
-      <p>Kenshikeza is </p>
+      <p>Kenshikeza, an innovative online portal, stands as the epitome of seamless travel experiences, <br />
+             offering a comprehensive platform that centers around tourism and the sale of related products. <br />
+             Whether you're seeking enchanting destinations, curated travel packages, or memorable excursions,<br />
+             Kenshikeza caters to your wanderlust with a diverse array of options. Beyond its focus on travel, <br />
+              the portal elevates the user experience by facilitating hassle-free bookings for top-notch restaurants <br />
+             and accommodations. With Kenshikeza, users can effortlessly plan their entire journey, from exploring <br />
+             captivating landmarks to securing reservations at the finest hotels and dining establishments. <br />
+         </p>
     </div>
     </div>
     
