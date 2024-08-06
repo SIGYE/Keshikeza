1989ce47b29fe6fb26841803bb68b120ef860b5d
Mwesigye Linda <lindamwesigye2@gmail.com>
2024-08-06 20:56:48 +0200

    services redo

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,26 +1,12 @@
 import React from 'react'
-import {FaGlobe} from "react-icons/fa"
-import Line4 from "../../assets/Line 4.png"
-import './Services.css'
 
 const Services = () => {
   return (
-   <>
-    <div className="service">
-        <img src={Line4} alt="" className="one" />
-        <h2>Services</h2>
-        <img src={Line4} alt="" className="two" />
-        <h1>Our Services</h1>
+    <div className='max-w-7xl  mx-auto px-4 py-9 '>
+      <div className="text-center">
+        <h2 className="text-3xl font-bold text-gray-800">Our Services</h2>
+      </div>
     </div>
-   <div className="service-cards">
-    <FaGlobe color='green' width={100}/>
-    <h1>Road Trips</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
-         Aliquam, ab ad incidunt consectetur neque animi aut officia <br />
-         libero temporibus unde, dolore maiores ex commodi! Alias <br />
-          ad possimus pariatur saepe necessitatibus.</p>
-   </div>
-   </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
